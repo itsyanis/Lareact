10c8f29ad4f9a495038b180ae08573805bbb845b
Yanis MAAFI <yanis.maafi@viacesi.fr>
2021-12-28 21:53:33 +0100

    Integrate React JS

---
+++ resources/js/components/Index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Index() {
+    return (
+        <>
+        </>
+    );
+}
+
+export default Index;
+
+if (document.getElementById('root')) {
+    ReactDOM.render(<Index />, document.getElementById('root'));
+}
